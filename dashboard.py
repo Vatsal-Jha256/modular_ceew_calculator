0de9d5c9303f1e9e16dd50e5ec3e7e57588ca963
VatsalJha <119221761+Vatsal-Jha256@users.noreply.github.com>
2024-07-24 10:00:01 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,8 @@ from helper import *
 from solar_grid_dg import calculate_solar_grid_dg_costs
 from solar_grid import calculate_solar_grid_costs
 from grid_dg import calculate_grid_dg_costs
-from 
+from solar_grid_bess_analyse import calculate_solar_grid_bess_costs
+from solar_grid_bess_optimize import optimize_solar_grid_bess_costs
 
 
 
